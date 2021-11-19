847f081afaeeb5982a0d3b25dfe7f851fb325cf6
Henrique <91853975+CHPM1000@users.noreply.github.com>
2021-11-19 04:46:42 -0300

    Update conversor de moeda.py

--- conversor de moeda.py
+++ conversor de moeda.py
@@ -4,4 +4,4 @@ Euro= real/6.22
 Dólar_Canadense= real/4.38
 print('Com ${} você pode comprar\nUS$ {:.4}\nEURO {:.4}\nC$ {:.4}'.format(real,Dólar,Euro,Dólar_Canadense))
 #Coloco a descrição da moeda que quero e logo após coloco o valor de quanto custa o valor da nossa moeda,na moeda deles
-#exemplo 1dolar;5.50 nunca colocar virgula, já {:.3} é quantos digitos quero ter no valor!!!
+#exemplo 1dolar;5.50   :).
