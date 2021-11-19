eb05a103d00f30171e8012a1f5a5282ab78a04c6
Henrique <91853975+CHPM1000@users.noreply.github.com>
2021-11-19 04:44:54 -0300

    rename média escolar.py

--- média escolar.py
+++ média escolar.py
@@ -2,5 +2,3 @@ nota1=float(input('Primeira nota:'))
 nota2=float(input('Segunda nota:'))
 Média=(nota1+nota2) / 2
 print('A média entre {:.1f} e {:.1f} é igual a\n {}'.format(nota1,nota2,Média))
-
-#{:.1f} exemplo depois do .coloque apenas 1 digito!!!
