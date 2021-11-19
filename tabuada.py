49527d68d231c45bfd2384ce31503b23b23afc1a
Henrique <91853975+CHPM1000@users.noreply.github.com>
2021-11-19 04:45:40 -0300

    exclua

--- tabuada.py
+++
@@ -1,3 +0,0 @@
-v=int(input('Digite um número pra ver a TABUADA:'))
-print('{} x {:2} = {}'.format(v,1, v*1))
-#{:2} seria corrigir espaçamento, então v valor colocado, x o numero que na ordem, depois coloco a conta, v*1 ;)
